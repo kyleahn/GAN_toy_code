80483ca5a58b80b0f9ff1e4ede270dc0d752a103
Beomjin Ahn <tigersdennn@gmail.com>
2018-06-07 14:03:19 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+iteration = 100
+
+for _ in range(iteration):
+
+    k = 10
+    for _ in range(k):
+        #train D
+        pass
+
+    #train G
+    pass
